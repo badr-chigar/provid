98d5bc4bcf9f06052118b8312aab56fa9bed711a
Badr Chigar <132501656+badr-chigar@users.noreply.github.com>
2026-06-01 03:02:14 +0100

    Add files via upload

---
+++ backend/src/main/java/ma/provid/ProvidApplication.java
@@ -0,0 +1,11 @@
+package ma.provid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvidApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProvidApplication.class, args);
+    }
+}
